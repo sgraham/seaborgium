d3d9f7614b7a78807767f0229b2d95195d61add8
Scott Graham <scottmg@chromium.org>
2014-05-22 17:32:46 -0700

    no debug/release subdirs

--- tools/regen.bat
+++ tools/regen.bat
@@ -1,12 +1,13 @@
 @echo off
+if x%1==x goto bad
 rmdir /s/q out &^
 mkdir out &&^
-mkdir out\Debug &&^
-mkdir out\Release &&^
 set CC=cl &&^
 set CXX=cl &&^
-cd out\Debug &&^
-cmake ..\.. -G Ninja -DCMAKE_BUILD_TYPE=Debug &&^
-cd ..\..\out\Release &&^
-cmake ..\.. -G Ninja -DCMAKE_BUILD_TYPE=Release &&^
-cd ..\..
+cd out &&^
+cmake .. -G Ninja -DCMAKE_BUILD_TYPE=%1 &&^
+cd ..
+goto :EOF
+
+:bad
+echo usage: tools\regen Debug^|Release
