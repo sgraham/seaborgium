a84952fad67c0551e9595ac452b546f98d75bb6d
Scott Graham <scottmg@chromium.org>
2014-06-12 11:57:46 -0700

    check for cl to avoid cmake spew

--- tools/regen.bat
+++ tools/regen.bat
@@ -1,5 +1,9 @@
 @echo off
+setlocal
 if x%1==x goto bad
+set CLPATH=
+for %%i in (cl.exe) do set CLPATH="%%~$PATH:i"
+if x%CLPATH%==x goto nocl
 rmdir /s/q out &^
 mkdir out &&^
 set CC=cl &&^
@@ -11,3 +15,8 @@ goto :EOF
 
 :bad
 echo usage: tools\regen Debug^|Release
+goto :EOF
+
+:nocl
+echo cl.exe not found, must run something like "C:\Program Files (x86)\Microsoft Visual Studio 12.0\vc\vcvarsall.bat" first.
+goto :EOF
